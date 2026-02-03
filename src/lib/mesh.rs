9162b68f8e3c84b44c3829ee610df3189c8f5601
Lucas <duanemperorph@gmail.com>
2026-02-03 13:17:55 -0800

    added: split line mesh mutation

--- src/lib/mesh.rs
+++ src/lib/mesh.rs
@@ -131,6 +131,17 @@ impl Mesh {
             .collect();
     }
 
+    pub fn lines_to_vert_pairs_from_list(&self, lines: &[Line]) -> Vec<(Vec3, Vec3)> {
+        lines
+            .iter()
+            .map(|line| {
+                let v1 = *self.verticies.get(line.0).unwrap_or(&Vec3::ZERO);
+                let v2 = *self.verticies.get(line.1).unwrap_or(&Vec3::ZERO);
+                (v1, v2)
+            })
+            .collect()
+    }
+
     pub fn polys_to_triangle_indicies(&self) -> Vec<VertIndex> {
         self.polys
             .iter()
@@ -302,6 +313,78 @@ impl Mesh {
             v.x * radians.cos() - v.y * radians.sin(),
             v.x * radians.sin() + v.y * radians.cos(),
         )
+    }
+}
+
+//
+// Addative mutations (split)
+//
+
+impl Mesh {
+    pub fn split_lines(&mut self, line_indicies: &HashSet<LineIndex>) {
+        let lines_ordered: Vec<Line> = self
+            .lines
+            .iter()
+            .enumerate()
+            .filter(|(i, _)| line_indicies.contains(i))
+            .map(|(_, &l)| l)
+            .collect();
+
+        let center_vecs: Vec<Vec3> = self
+            .lines_to_vert_pairs_from_list(&lines_ordered)
+            .iter()
+            .map(|&(v1, v2)| (v1 + v2) / 2.0)
+            .collect();
+
+        self.verticies.extend(center_vecs);
+
+        // remove old lines
+        self.lines = self
+            .lines
+            .drain(..)
+            .into_iter()
+            .enumerate()
+            .filter(|(i, _)| line_indicies.contains(i))
+            .map(|(_, l)| l)
+            .collect();
+
+        // add new lines and update polys
+        let new_verts_start_index = self.verticies.len();
+
+        for (i, old_line) in lines_ordered.iter().enumerate() {
+            let new_vert_index = i + new_verts_start_index;
+            let new_line_1 = (old_line.0, new_vert_index);
+            self.lines.push(new_line_1);
+            let new_line_2 = (new_vert_index, old_line.1);
+            self.lines.push(new_line_2);
+            self.update_polys_with_new_vert_between_old_verts(old_line, new_vert_index);
+        }
+    }
+
+    fn update_polys_with_new_vert_between_old_verts(
+        &mut self,
+        old_verts: &Line,
+        new_vert: VertIndex,
+    ) {
+        for poly in self.polys.iter_mut() {
+            if poly.len() < 3 {
+                continue;
+            }
+            let first_vert_index = poly.first().unwrap();
+            let last_vert_index = poly.last().unwrap();
+            let wrap_around_pair = [*last_vert_index, *first_vert_index];
+            let index = poly
+                .windows(2)
+                .chain(std::iter::once(wrap_around_pair.as_slice()))
+                .position(|pair| {
+                    (pair[0] == old_verts.0 && pair[1] == old_verts.1)
+                        || (pair[0] == old_verts.1 && pair[1] == old_verts.0)
+                });
+
+            if let Some(index) = index {
+                poly.insert(index, new_vert);
+            }
+        }
     }
 }
 
