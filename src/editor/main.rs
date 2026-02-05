ec4bded4191b8153372da59164a37bd74f199a50
Lucas <duanemperorph@gmail.com>
2026-02-05 12:13:19 -0800

    refactored: module root to use new MeshDocument interface

--- src/editor/main.rs
+++ src/editor/main.rs
@@ -41,40 +41,40 @@ use mesh_document::MeshDocument;
 
 #[macroquad::main("Mesh Editor")]
 async fn main() {
-    let mut current_mesh = MeshData::new_tapered_box();
+    let mut document = MeshDocument::with_mesh(MeshData::new_tapered_box());
     let mut editor_state = EditorState::new();
 
     loop {
         let panes = Panes::calc_from_screen_dims();
 
-        handle_keyboard_commands(&mut editor_state, &mut current_mesh);
-        handle_mouse_commands(&mut editor_state, &current_mesh, &panes);
+        handle_keyboard_commands(&mut editor_state, document.current_mesh_mut());
+        handle_mouse_commands(&mut editor_state, document.current_mesh(), &panes);
 
         clear_background(BLACK);
 
         if *editor_state.viewer_mode() == ViewerMode::EditorPanels {
             render_editor_pane_viewport(
                 editor_state.panel_state_xz(),
-                &current_mesh,
+                document.current_mesh(),
                 &editor_state.selection(),
                 panes.left_viewport(),
             );
             render_editor_pane_viewport(
                 editor_state.panel_state_yz(),
-                &current_mesh,
+                document.current_mesh(),
                 &editor_state.selection(),
                 panes.top_right_viewport(),
             );
             render_editor_pane_viewport(
                 editor_state.panel_state_xy(),
-                &current_mesh,
+                document.current_mesh(),
                 &editor_state.selection(),
                 panes.bottom_right_viewport(),
             );
         } else {
             render_editor_pane_viewport(
                 editor_state.panel_state_free_cam(),
-                &current_mesh,
+                document.current_mesh(),
                 &editor_state.selection(),
                 panes.full_content_viewport(),
             );
@@ -86,7 +86,7 @@ async fn main() {
             panes.draw_bottom_border();
         }
         add_panel_ui_controls(&mut editor_state, &panes);
-        draw_status_text(&editor_state, &current_mesh);
+        draw_status_text(&editor_state, document.current_mesh());
 
         next_frame().await
     }
